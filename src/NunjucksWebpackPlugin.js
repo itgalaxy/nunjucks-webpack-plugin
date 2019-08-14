2a4da13305b1b5653b1f4f929317cc8ea7f6cc50
Tony Le <46855519+let00@users.noreply.github.com>
2019-08-14 10:15:23 -0400

    Update error throw

--- src/NunjucksWebpackPlugin.js
+++ src/NunjucksWebpackPlugin.js
@@ -25,7 +25,7 @@ class NunjucksWebpackPlugin {
       !Array.isArray(this.options.templates) ||
       this.options.templates.length === 0
     ) {
-      throw new Error("Options `templates` must be an non-empty array");
+      throw new Error("Options `templates` must be a non-empty array");
     }
   }
 
